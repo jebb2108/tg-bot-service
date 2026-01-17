a9974974987256cc6d74a09fb10426bfa89f54f7
jebb <gabouchard2002@gmail.com>
2026-01-17 17:49:57 +0300

    bug fix

--- src/routers/callback_handlers/main_menu_cb_handler.py
+++ src/routers/callback_handlers/main_menu_cb_handler.py
@@ -146,7 +146,7 @@ async def profile_handler(callback: CallbackQuery, state: FSMContext):
         nickname = data.get("nickname", callback.from_user.username)
         sidebar = "=" * (15 - len(nickname))
         formated_nickname = sidebar + " " + nickname + " " + sidebar
-        topics = list([TRANSCRIPTIONS["topics"][topic][lang_code] for topic in data.get('topics').split(', ')])
+        topics = list([TRANSCRIPTIONS["topics"][topic][lang_code] for topic in data.get('topics')])
         msg = MESSAGES["user_info"][lang_code].format(
             nickname=formated_nickname,
             age=data.get("age", 'not specified'),
